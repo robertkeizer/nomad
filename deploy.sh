019dd870538e91f177f53c7285959aad017b8798
Tracey Jaquith <tracey@archive.org>
2022-01-03 19:48:11 -0800

    missed one replace

--- deploy.sh
+++ deploy.sh
@@ -142,7 +142,7 @@ function github-setup() {
   export CI_REGISTRY=ghcr.io
 
   # eg: ghcr.io/internetarchive/dyno:main  (registry image)
-  export CI_GITHUB_IMAGE="${REGISTRY?}/${GITHUB_REPOSITORY?}:${GITHUB_REF_NAME?}"
+  export CI_GITHUB_IMAGE="${CI_REGISTRY?}/${GITHUB_REPOSITORY?}:${GITHUB_REF_NAME?}"
 
   # eg: dyno  (project name)
   export CI_PROJECT_NAME=$(basename "${GITHUB_REPOSITORY?}")
