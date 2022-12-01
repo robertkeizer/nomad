f37cef39496947aa71c3bbd4fb9a21aa40040670
Tracey Jaquith <tracey@archive.org>
2022-12-01 01:59:09 -0800

    updated deprecated method

--- etc/caddy/echo.js
+++ etc/caddy/echo.js
@@ -2,7 +2,7 @@
 
 // basic TCP "echo" server, from https://deno.land/manual@v1.26.1/examples/tcp_echo
 
-import { copy } from "https://deno.land/std@0.159.0/streams/conversion.ts"
+import { copy } from 'https://deno.land/std/streams/copy.ts'
 const listener = Deno.listen({ port: 32123 })
 
 for await (const conn of listener) copy(conn, conn).finally(() => conn.close())
