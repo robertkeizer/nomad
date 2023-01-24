f7d5c3931ffef9b31da7fdf87770ced7d9229473
Tracey Jaquith <tracey@archive.org>
2023-01-24 13:36:17 -0800

    switch off deprecated apt-key

--- bin/install-docker-ce.sh
+++ bin/install-docker-ce.sh
@@ -1,28 +1,20 @@
 #!/bin/bash -ex
 
 # Installs latest stable docker-ce version for current ubuntu OS.
-# Should be andy/tracey compatible -- ie: should be same version.
-VER="=5:19.03.13~3-0~ubuntu-focal"
-VER=
 
 [ "$( docker -v 2> /dev/null )" = "" ]  ||  echo 'docker already installed - exiting'
 [ "$( docker -v 2> /dev/null )" = "" ]  ||  exit 0
 
 sudo apt-get -yqq update
-sudo apt-get -yqq install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  software-properties-common
+sudo apt-get -yqq install   ca-certificates   curl   gnupg
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable" | sudo tee /etc/apt/sources.list.d/download_docker_com_linux_ubuntu.list
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get -yqq update
 
-sudo apt-get -yqq install docker-ce$VER
+sudo apt-get -yqq install docker-ce
